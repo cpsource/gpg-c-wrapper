745cd5bef728b1092414e8de635fe951807c6433
page.cal@gmail.com <cal.page@digi.com>
2016-08-26 09:56:28 -0400

    Add push signing instruction to git

--- developer-setup.sh
+++ developer-setup.sh
@@ -2,4 +2,7 @@
 # used only once to allow developers to do
 # a signed push to github, ie git commit -S -e
 exit 0
+git config user.name page.cal@gmail.com
 git config commit.gpgsign true
+git config user.signingkey 928DD135
+
